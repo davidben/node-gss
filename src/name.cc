2198cea761b2141934406fecf5895828c8c19c15
David Benjamin <davidben@mit.edu>
2013-06-06 03:36:29 -0400

    Wrap more things for managing names

--- src/name.cc
+++ src/name.cc
@@ -37,7 +37,6 @@ v8::Handle<v8::Value> DisplayName(const v8::Arguments& args) {
 
   gss_buffer_desc buffer;
   gss_OID oid;
-
   OM_uint32 minor;
   OM_uint32 major = gss_display_name(&minor, name->get(), &buffer, &oid);
 
@@ -49,9 +48,80 @@ v8::Handle<v8::Value> DisplayName(const v8::Arguments& args) {
                              buffer.length)->handle_);
   ret->Set(v8::String::NewSymbol("oid"),
            OidHandle::New(oid)->handle_);
-
   gss_release_buffer(NULL, &buffer);
+  return scope.Close(ret);
+}
 
+v8::Handle<v8::Value> CompareName(const v8::Arguments& args) {
+  v8::HandleScope scope;
+
+  if (args.Length() < 2) {
+    v8::ThrowException(v8::Exception::TypeError(v8::String::New(
+        "Wrong number of arguments")));
+    return scope.Close(v8::Undefined());
+  }
+
+  HANDLE_ARGUMENT(0, NameHandle, name1);
+  HANDLE_ARGUMENT(1, NameHandle, name2);
+
+  int name_equal;
+  OM_uint32 minor;
+  OM_uint32 major = gss_compare_name(&minor,
+                                     name1->get(), name2->get(), &name_equal);
+
+  v8::Local<v8::Object> ret = v8::Object::New();
+  ret->Set(v8::String::NewSymbol("major"), v8::Integer::New(major));
+  ret->Set(v8::String::NewSymbol("minor"), v8::Integer::New(minor));
+  ret->Set(v8::String::NewSymbol("nameEqual"), v8::Integer::New(name_equal));
+  return scope.Close(ret);
+}
+
+v8::Handle<v8::Value> CanonicalizeName(const v8::Arguments& args) {
+  v8::HandleScope scope;
+
+  if (args.Length() < 3) {
+    v8::ThrowException(v8::Exception::TypeError(v8::String::New(
+        "Wrong number of arguments")));
+    return scope.Close(v8::Undefined());
+  }
+
+  HANDLE_ARGUMENT(0, NameHandle, name);
+  HANDLE_ARGUMENT(1, OidHandle, oid);
+  HANDLE_ARGUMENT(2, NameHandle, output);
+
+  OM_uint32 minor;
+  OM_uint32 major = gss_canonicalize_name(&minor,
+                                          name->get(), oid->get(),
+                                          &output->get());
+
+  v8::Local<v8::Object> ret = v8::Object::New();
+  ret->Set(v8::String::NewSymbol("major"), v8::Integer::New(major));
+  ret->Set(v8::String::NewSymbol("minor"), v8::Integer::New(minor));
+  return scope.Close(ret);
+}
+
+v8::Handle<v8::Value> ExportName(const v8::Arguments& args) {
+  v8::HandleScope scope;
+
+  if (args.Length() < 1) {
+    v8::ThrowException(v8::Exception::TypeError(v8::String::New(
+        "Wrong number of arguments")));
+    return scope.Close(v8::Undefined());
+  }
+
+  HANDLE_ARGUMENT(0, NameHandle, name);
+
+  gss_buffer_desc buffer;
+  OM_uint32 minor;
+  OM_uint32 major = gss_export_name(&minor, name->get(), &buffer);
+
+  v8::Local<v8::Object> ret = v8::Object::New();
+  ret->Set(v8::String::NewSymbol("major"), v8::Integer::New(major));
+  ret->Set(v8::String::NewSymbol("minor"), v8::Integer::New(minor));
+  ret->Set(v8::String::NewSymbol("buffer"),
+           node::Buffer::New((const char*)buffer.value,
+                             buffer.length)->handle_);
+  gss_release_buffer(NULL, &buffer);
   return scope.Close(ret);
 }
 
@@ -60,6 +130,12 @@ void NameInit(v8::Handle<v8::Object> exports) {
                v8::FunctionTemplate::New(ImportName)->GetFunction());
   exports->Set(v8::String::NewSymbol("displayName"),
                v8::FunctionTemplate::New(DisplayName)->GetFunction());
+  exports->Set(v8::String::NewSymbol("compareName"),
+               v8::FunctionTemplate::New(CompareName)->GetFunction());
+  exports->Set(v8::String::NewSymbol("canonicalizeName"),
+               v8::FunctionTemplate::New(CanonicalizeName)->GetFunction());
+  exports->Set(v8::String::NewSymbol("exportName"),
+               v8::FunctionTemplate::New(ExportName)->GetFunction());
 }
 
 }  // namespace node_gss
